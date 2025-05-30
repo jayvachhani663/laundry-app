7dc25ebbe22eed193b31355ae92cbda8070117cf
jay vachhani <vachhanijay6631@gmail.com>
2025-05-30 10:19:28 +0530

    Added SignIn page, removed Home route, updated layout and routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import Signin from './pages/SignIn'; 
 import Layout from './components/Layout';
 
@@ -8,7 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
           <Route path="signin" element={<Signin />} /> 
         </Route>
       </Routes>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,12 @@ const Layout = () => {
   return (
     <>
       <nav style={{ padding: "1rem", background: "#eee" }}>
-        <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+        <Link to="#" style={{ marginRight: "1rem" }}>Home</Link>
+        <Link to="#" style={{ marginRight: "1rem" }}>Pricing</Link>
+        <Link to="#" style={{ marginRight: "1rem" }}>Career</Link>
         <Link to="/signin">Sign In</Link>
       </nav>
+
       <main style={{ padding: "1rem" }}>
         <Outlet />
       </main>
